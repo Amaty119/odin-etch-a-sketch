aa305891f65d39e9ddeedd9d7b663b14fb677838
Amy Kim <amy.k7019@gmail.com>
2022-08-14 03:04:53 +0900

    added adjustable grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,54 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
+let size = document.querySelector("#slide");
+let value = document.querySelector("#value");
+value.textContent = size.value;
 
 
-function createGrid (gridSize){
+function createGrid (gridSize) {
     for(let i = 0; i < gridSize; i++) {
         const divs1 = document.createElement("div");
         divs1.className = "outerClass";
+        divs1.style.height = 40/gridSize + "vh";
         for(let j = 0; j < gridSize; j++) {
             const divs2 = document.createElement("div");
             divs2.className = "innerClass";
             divs1.appendChild(divs2);
+            divs2.style.width = 40/gridSize + "vh";
+            divs2.style.height = 40/gridSize + "vh";
         }
         container.appendChild(divs1);
     }
     
 }
 
-createGrid(4);
+function clearGrid() {
+    const gridRows = document.querySelectorAll(".outerClass");
+    const gridBoxes = document.querySelectorAll(".innerClass");
+    gridBoxes.forEach((gridBox) => {
+        gridBox.remove();
+    });
+    gridRows.forEach((row) => {
+        row.remove();
+    });
+}
+
+function draw() {
+    const gridBoxes = document.querySelectorAll(".innerClass");
+    gridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener("mouseover", (e) => {
+            if(e.buttons == 1) {
+                gridBox.classList.add("active");
+            }
+        });
+    });
+}
+
+size.addEventListener("click", () => {
+    clearGrid();
+    createGrid(size.value);
+    value.textContent = size.value;
+    draw();
+})
+
+createGrid(size.value);
+draw();
